c79b68af02e419520211752a3218b7567d4b2adc
Ahmadtalat111 <Ahmadtalat111@yahoo.com>
2020-06-12 16:54:46 +0200

    Initial commit

---
+++ app/src/main/java/com/ahmadtalat/allaelossool/StartFragment.kt
@@ -0,0 +1,20 @@
+package com.ahmadtalat.allaelossool
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.ahmadtalat.allaelossool.databinding.FragmentStartBinding
+
+
+class StartFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentStartBinding>(inflater,
+            R.layout.fragment_start,container,false)
+        return binding.root
+    }
+
+}
